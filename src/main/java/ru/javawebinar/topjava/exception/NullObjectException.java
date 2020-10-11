5d1cd7becf33cdd22553a58db4d243d794b65d3e
rlrio <svetlana.kras881@gmail.com>
2020-10-11 11:34:16 +0300

    HW1 patch

---
+++ src/main/java/ru/javawebinar/topjava/exception/NullObjectException.java
@@ -0,0 +1,7 @@
+package ru.javawebinar.topjava.exception;
+
+public class NullObjectException extends RuntimeException {
+    public NullObjectException(String message) {
+        super(message);
+    }
+}
